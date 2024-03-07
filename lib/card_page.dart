a33fc37946f0f1d910e54ba908f0e02808d40c6c
prats3992 <pratham3992@gmail.com>
2024-03-07 15:38:45 +0530

    My first app

--- lib/card_page.dart
+++ lib/card_page.dart
@@ -1,6 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:ui' as ui;
+
+import 'package:share_plus/share_plus.dart';
 
 class CardPage extends StatelessWidget {
   CardPage({super.key, required this.name, required this.birthDay});
@@ -13,14 +21,16 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var myWidth = MediaQuery.of(context).size.width - 20;
+    int ageYears = (DateTime.now().year - birthDay.year);
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple.shade200,
+        backgroundColor: Colors.blue.shade700,
         title: Text("Birthday Card Generator"),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               decoration: BoxDecoration(
@@ -29,7 +39,42 @@ class CardPage extends StatelessWidget {
               child: RepaintBoundary(
                 key: genKey,
                 child: Stack(
-                  children: [],
+                  children: [
+                    SizedBox(
+                      height: myWidth,
+                      width: myWidth,
+                      child: Container(
+                        color: Colors.pink.shade400,
+                      ),
+                    ),
+                    Positioned(
+                        top: 100,
+                        left: 10,
+                        child: Text(
+                          "Happy Birthday",
+                          style: TextStyle(
+                              fontSize: 25.0, color: Colors.blue.shade900),
+                        )),
+                    Positioned(
+                        top: 135,
+                        left: 10,
+                        child: Text(
+                          name,
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blue.shade900,
+                              fontWeight: FontWeight.bold),
+                        )),
+                        Positioned(
+                        top: 165,
+                        left: 10,
+                        child: Text(
+                          "You are $ageYears years old",
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blue.shade900,),
+                        )),
+                  ],
                 ),
               ),
             ),
@@ -38,7 +83,23 @@ class CardPage extends StatelessWidget {
             ),
             ElevatedButton(
               child: const Text("Share Card"),
-              onPressed: ()  {},
+              onPressed: () async {
+                RenderRepaintBoundary? boundary = genKey.currentContext!.findRenderObject() as RenderRepaintBoundary?;
+
+                if (boundary == null) {
+                  return;
+                }
+
+                ui.Image image = await boundary.toImage(pixelRatio: 4.0);
+                ByteData? bytes = await image.toByteData(format: ui.ImageByteFormat.png);
+                Uint8List actualImage = bytes!.buffer.asUint8List();
+                
+                Directory tempDir = await Directory.systemTemp.createTemp();
+                File file = File('${tempDir.path}/image.png');
+                await file.writeAsBytes(actualImage);
+
+                await Share.shareXFiles([XFile("${tempDir.path}/image.png")]);
+              },
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:wkshp_app/card_page.dart';
 
 // the main function, called when the app is run.
 void main() {
@@ -14,7 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // the build function returns what is shown on screen.
 
-    return const MaterialApp( // An app with the "Material" UI design scheme.
+    return const MaterialApp(
+      // An app with the "Material" UI design scheme.
       home: MyHomePage(),
     );
   }
@@ -30,17 +34,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController nameController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2000),
+        lastDate: DateTime.now());
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
     // A simple app with a purple app bar and a centered text.
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pink.shade200,
+        backgroundColor: Colors.blue.shade700,
         title: const Text("Birthday Card Generator"),
       ),
-      body: const Center(
-        child: Text("Hello World!"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your name',
+                    labelText: 'Name',
+                  ),
+                )),
+            Text("${selectedDate.toLocal()}".split(' ')[0]), // '2021-06-23'
+            ElevatedButton(
+                onPressed: () async {
+                  await _selectDate(context);
+                },
+                child: Text("Choose date")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CardPage(
+                              name: nameController.text,
+                              birthDay: selectedDate)));
+                },
+                child: Text("Generate card")),
+          ],
+        ),
       ), // The comma at the end makes everything look nicer.
     );
   }
